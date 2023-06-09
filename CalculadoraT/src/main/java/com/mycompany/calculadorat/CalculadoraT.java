4a61dc7bdee6e6d0e8a72d2fd3704015e9fc366b
MontserratPalma <135903590+MontserratPalma@users.noreply.github.com>
2023-06-08 19:06:06 -0600

    Practica Calculadora

---
+++ CalculadoraT/src/main/java/com/mycompany/calculadorat/CalculadoraT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorat;
+
+import Practica8.Calculadora;
+
+
+
+/**
+ *
+ * @author Monts
+ */
+public class CalculadoraT {
+
+    public static void main(String[] args) {
+        Calculadora v1 = new Calculadora();
+        v1.setVisible(true);
+    }
+}
